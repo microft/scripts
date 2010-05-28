91f681057e671d024ffa04d1eadc509582139c35
Luís Miguel Braga <human@microft.org>
2010-05-28 18:26:58 +0100

    added some explenatory comments

--- browser-chooser.sh
+++ browser-chooser.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#
+# If you make this script your default browser when you click on a link
+# it'll look at running browsers and open the link on the first available one
+#
+# If there isn't a browser running, it'll launch the last one
+#
+
 # The last option should be the default. To be used if none are running
 BROWSERS=( firefox chromium-browser opera google-chrome x-www-browser )
+
 for browser in ${BROWSERS[@]}; do
   RUNNING=$(pgrep -u $USER -f $browser);
   if [ "$RUNNING" ]; then
