5536311af50070a8ee68ea894636c1cc2cdb539d
Luis Miguel Braga <root@microft.org>
2010-08-25 02:33:24 +0100

    added a little more logic.

--- tvcalendar2csv.pl
+++ tvcalendar2csv.pl
@@ -7,6 +7,13 @@ use Data::ICal;
 my $calendar_url = "http://www.pogdesign.co.uk/cat/download_ics/2af7747583b17bb008bf5c5211305089";
 my %shows = ();
 
+while(<>){
+  chomp;
+  (my $name, my $quality) = split(/;/,$_);
+  $shows{$name} = $quality;
+} 
+
+
 my $ua = LWP::UserAgent->new();
 my $ical = $ua->get($calendar_url)->content;
 my $entries = Data::ICal->new(data => $ical)->entries;
@@ -14,11 +21,11 @@ for my $entry (@{$entries}){
   my %properties = %{$entry->properties()};
   my $value = $properties{'categories'}[0]->value;
   $value =~ s/\s*(.+)\s+Episodes, TV Shows/$1/;
-  $shows{$value} = 1;
+  $shows{$value} = '720p';
 }
 
 for my $show (keys %shows){
-  print "$show;720p;\n";
+  print "$show;$shows{$show};\n";
 }
 
 
