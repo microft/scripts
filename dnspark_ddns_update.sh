bb0fa6c0ff5f2c511cd1ed8928df7273edd3c957
Luis Miguel Braga <root@microft.org>
2013-03-20 16:13:09 +0000

    added a test to see if the IP actually changed before trying to update the record

--- dnspark_ddns_update.sh
+++ dnspark_ddns_update.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# this script updates a dynamic dns register at dns park
 HOSTNAME=$1
 IP=$2
 USERNAME=$3
 PASSWORD=$4
 
-CURL=`which curl`
-$CURL "https://$USERNAME:$PASSWORD@www.dnspark.net/api/dynamic/update.php?hostname=$HOSTNAME&ip=$IP&mx=ON&mxpri=5"
+CURRENTIP=`host $HOSTNAME | grep -o "\([0-9]\+\.\)\{3\}[0-9]\+"`
 
+if [ "$IP" != "$CURRENTIP" ]
+then
+    CURL=`which curl`
+    $CURL "https://$USERNAME:$PASSWORD@www.dnspark.net/api/dynamic/update.php?hostname=$HOSTNAME&ip=$IP&mx=ON&mxpri=5"
+else
+    echo 'no change'
+fi
